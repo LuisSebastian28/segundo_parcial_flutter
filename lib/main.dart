b51997bb638eece5111e97641787ff52c943b27b
LuisSebastian28 <sabestech@gmail.com>
2023-10-05 13:28:52 -0400

    segundo parcial commit

---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginScreen(),
+        '/recover_password': (context) => RecoverPasswordScreen(),
+        '/user_profile': (context) => UserProfileScreen(),
+      },
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Login')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Correo'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Contraseña'),
+              obscureText: true,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/user_profile');
+              },
+              child: Text('Iniciar Sesión'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/recover_password');
+              },
+              child: Text('Recuperar Contraseña'),
+            ),
+            Text('O autentificarse con:'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/');
+              },
+              child: Text('Facebook'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/');
+              },
+              child: Text('Gmail'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RecoverPasswordScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Recuperar Contraseña')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Contraseña Actual'),
+              obscureText: true,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Nueva Contraseña'),
+              obscureText: true,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Repetir Contraseña'),
+              obscureText: true,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Agregar lógica para cambiar la contraseña aquí
+              },
+              child: Text('Cambiar Contraseña'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class UserProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Perfil de Usuario')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Agregar la imagen del usuario aquí
+            Text('Nombre del Usuario'),
+            Text('Correo'),
+            ElevatedButton(
+              onPressed: () {
+                // Agregar lógica para editar el perfil aquí
+              },
+              child: Text('Editar Perfil'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
